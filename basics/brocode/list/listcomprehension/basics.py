4edbe14e6813b1d4e7fd5614252c7b9d4b790543
Pragyan2428 <singhpragyan2911@gmail.com>
2026-08-01 04:33:55 +0530

    list comprehension added

---
+++ basics/brocode/list/listcomprehension/basics.py
@@ -0,0 +1,4 @@
+doubles = [x*2 for x in range(1,11) if x%2==0]
+print(doubles)
+
+# Syntax:expression for value in rnage if condition
--- basics/brocode/membership operators/check_email.py
+++ basics/brocode/membership operators/check_email.py
@@ -2,6 +2,7 @@ def main():
     email = input("Enter you mail: ")
     if '@' and '.' in email:
         print("Your email is valid.")
-    
+    else:
+        print("Your email is not valid.")
 
 main()
